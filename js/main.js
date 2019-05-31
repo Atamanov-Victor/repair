e56a48cd075c13069e87a59469f2046993f362f7
Scandalist <Scandalist@users.noreply.github.com>
2019-05-31 20:36:21 +0300

    Modal window improved

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ button.addEventListener('click', function(){
     // console.log(modal.classList)
     modal.classList.add('modal_active')
     // console.log(modal.classList)
-    setTimeout(function(){
+    var leave = setTimeout(function(){
         modal.classList.remove('modal_active')
-     }, 5000)
+        clearTimeout(leave);
+     }, 5000) 
 })
 
 close.addEventListener('click', function(){
