99465e1182f9786a6f70414a6b3f10f60acbfa52
Scandalist <loyces@yandex.ru>
2019-06-06 10:49:28 +0300

    Changed slider to Owl-Carousel

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,45 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: $('.arrows__left'),
-        nextArrow: $('.arrows__right'),
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
+// $(document).ready(function(){
+//     $('.slider').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         prevArrow: $('.arrows__left'),
+//         nextArrow: $('.arrows__right'),
+//         responsive: [
+//             {
+//                 breakpoint: 1280,
+//                 settings: {
+//                     slidesToShow: 2,
+//                     slidesToScroll: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 800,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     })
+// })
+
+$(document).ready(function() {
+    $('#carousel').owlCarousel({
+        loop:true, 
+        margin:30, 
+        nav:true, 
+        navText:[$('.arrows__left'), $('.arrows__right')],
+        dots: false,
+        responsive:{ 
+            0:{
+                items:1
             },
-            {
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            800:{
+                items:2
+            },
+            1280:{
+                items:3
             }
-        ]
-    })
-})
+        }
+    });
+});
