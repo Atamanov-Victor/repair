9a9627d2faf98ad3f984c24e05111dc147042123
Scandalist <loyces@yandex.ru>
2019-06-07 21:20:34 +0300

    Form validation added

---
+++ js/validation.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(".phone").mask("+7 (999) 999-9999")
+    $('#brif-form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            phone: {
+                required: true
+            },
+            email: {
+                required: true
+            }
+            
+        },
+        messages: {
+            name: {
+                required: "Введите своё имя",
+                minlength: jQuery.validator.format("Имя должно содержать как минимум {0} символа"),
+                maxlength: "Имя слишком длинное!"
+            },
+            phone: {
+                required: "Введите свой телефон"
+            },
+            email: {
+                required: "Введите свой email"
+            }
+        },
+        errorClass: "invalid",
+        errorElement: "em"
+        
+    })
+})
