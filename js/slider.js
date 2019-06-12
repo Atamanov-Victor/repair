1df6033368c293036dd0ac137d280c290331bfd1
Scandalist <loyces@yandex.ru>
2019-06-12 14:56:01 +0300

    Optimization

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,3 @@
-// $(document).ready(function(){
-//     $('.slider').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         prevArrow: $('.arrows__left'),
-//         nextArrow: $('.arrows__right'),
-//         responsive: [
-//             {
-//                 breakpoint: 1280,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 1
-//                 }
-//             },
-//             {
-//                 breakpoint: 800,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             }
-//         ]
-//     })
-// })
-
 $(document).ready(function() {
     $('#carousel').owlCarousel({
         loop:true, 
