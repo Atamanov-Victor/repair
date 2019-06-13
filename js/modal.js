4dddd76e0067572f31fb1329b9cd3a6a8df08070
Scandalist <loyces@yandex.ru>
2019-06-13 20:19:45 +0300

    Fixes

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
     var button = $('#button'); 
     var modal = $('#modal')
     var close = $('#close')
-    console.log(button);
+    var prices = $('.card__link')
+    prices.on('click', function() { 
+        modal.addClass('modal_active');
+        var leave = setTimeout(function(){
+            modal.removeClass('modal_active')
+            clearTimeout(leave);
+         }, 5000) 
+    })
     button.on('click', function(){
         modal.addClass('modal_active');
         var leave = setTimeout(function(){
@@ -13,5 +20,4 @@ $(document).ready(function(){
     close.on('click', function(){
         modal.removeClass('modal_active');
     })
-
 })
